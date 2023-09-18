1fb00a435665593898f8b85d27e0bd7c3152498d
unknown <devendra13.dc@gmail.com>
2023-09-18 15:54:21 +0545

    ....

--- app/api/tripPro/route.ts
+++ app/api/tripPro/route.ts
@@ -41,5 +41,12 @@ export async function POST(req: Request) {
     requestOptions
   );
   const data = await res.json();
-  return NextResponse.json({ data });
+  return NextResponse.json(data, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
 }
